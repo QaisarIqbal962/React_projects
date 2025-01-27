b3f748c6e6943f99d80803461a476172c9e3f154
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-01-27 11:27:01 +0500

    changes on jsx

--- 01_Counter/src/Components/Counter.jsx
+++ 01_Counter/src/Components/Counter.jsx
@@ -1,43 +1,45 @@
-import React, { useState } from "react";  
-import './Counter.css';  
+import React, { useState } from "react";
+import "./Counter.css";
 
-const Counter = () => {  
-    const [count, setCount] = useState(0);  
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
-    const increment = () => {  
-        if (count < 20) {  
-            setCount(count + 1);  
-        }  
-    };  
+  const increment = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
 
-    const decrement = () => {  
-        if (count > 0) {  
-            setCount(count - 1);  
-        }  
-    };  
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
-    const reset = () => {  
-        setCount(0);  
-    };  
+  const reset = () => {
+    setCount(0);
+  };
 
-    return (  
-        <div className="container">  
-            <div className="card">  
-                <h1 className="title">Counter: {count}</h1>  
-                <div className="button-container">  
-                    <button onClick={increment} className="button increment">  
-                        Plus  
-                    </button>  
-                    <button onClick={decrement} className="button decrement">  
-                        Minus  
-                    </button>  
-                    <button onClick={reset} className="button reset">  
-                        Reset  
-                    </button>  
-                </div>  
-            </div>  
-        </div>  
-    );  
-};  
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="title"> Counter: {count}</h1>
+        <div className="button-container">
+          <button onClick={increment} className="button increment">
+            Plus
+          </button>
 
-export default Counter;  
+          <button onClick={decrement} className="button decrement">
+            Minus
+          </button>
+
+          <button onClick={reset} className="button reset">
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
