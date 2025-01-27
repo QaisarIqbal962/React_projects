ffa80e366ea29cd52bfb6840d504a3b230c89463
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-01-27 11:51:52 +0500

    done

--- 01_Counter/src/Components/Counter.jsx
+++ 01_Counter/src/Components/Counter.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import "./Counter.css";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    if (count < 20) {
-      setCount(count + 1);
-    }
-  };
+//   const increment = () => {
+//     if (count < 20) {
+//       setCount(count + 1);
+//     }
+//   };
 
-  const decrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  };
+//   const decrement = () => {
+//     if (count > 0) {
+//       setCount(count - 1);
+//     }
+//   };
 
-  const reset = () => {
-    setCount(0);
-  };
+//   const reset = () => {
+//     setCount(0);
+//   };
+
+
 
   return (
     <div className="container">
